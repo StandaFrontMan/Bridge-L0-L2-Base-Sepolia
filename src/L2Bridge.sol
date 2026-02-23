ab36c5665e7b6784c2a0d4c12798e5caf90db5a8
stas <stas@192.168.1.4>
2026-02-23 17:02:57 +0100

    feat/ct: L2Bridge initial

---
+++ src/L2Bridge.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
+import {ReentrancyGuard} from "openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
+
+contract L2Bridge is Ownable, ReentrancyGuard {
+    constructor () Ownable(msg.sender) {}
+}

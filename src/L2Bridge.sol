67b9cf8c4e806122e4d3242d28dac4966d295f0f
stas <stas@192.168.1.4>
2026-02-23 17:07:32 +0100

    feat: pused logic

--- src/L2Bridge.sol
+++ src/L2Bridge.sol
@@ -7,11 +7,36 @@ import {ReentrancyGuard} from "openzeppelin-contracts/contracts/utils/Reentrancy
 contract L2Bridge is Ownable, ReentrancyGuard {
     address public relayer;
 
+    bool public paused; 
+
     error Unauthorized();
+    error ContractPaused();
 
     event RelayerSet(address indexed relayer);
+    event Paused();
+    event Unpaused();
+
+    /// @notice Ensures the contract is not paused before allowing bridge operations
+    /// @custom:throws ContractPaused if the contract is currently in paused state
+    modifier isContractPaused() {
+        _isContractPaused();
+        _;
+    }
 
     constructor () Ownable(msg.sender) {}
+
+    function _isContractPaused() internal view {
+        if (paused) revert ContractPaused();
+    }
+
+    function changeContractPause() external onlyOwner {
+        paused = !paused;
+        if (paused) {
+            emit Paused();
+        } else {
+            emit Unpaused();
+        }
+    }
 
     function setRelayer(address _relayer) external onlyOwner {
         relayer = _relayer;

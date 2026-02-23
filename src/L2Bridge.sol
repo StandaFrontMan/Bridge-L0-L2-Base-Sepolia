b4aadf1d3a4ba1946105fa7964a9e8532fa41693
stas <stas@192.168.1.4>
2026-02-23 17:05:18 +0100

    feat/ct: relayer address logic

--- src/L2Bridge.sol
+++ src/L2Bridge.sol
@@ -5,5 +5,16 @@ import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
 import {ReentrancyGuard} from "openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
 
 contract L2Bridge is Ownable, ReentrancyGuard {
+    address public relayer;
+
+    error Unauthorized();
+
+    event RelayerSet(address indexed relayer);
+
     constructor () Ownable(msg.sender) {}
+
+    function setRelayer(address _relayer) external onlyOwner {
+        relayer = _relayer;
+        emit RelayerSet(_relayer);
+    }
 }
